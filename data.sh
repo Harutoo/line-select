9fd9dda5a2891aa1df13d62ecc33438cebe84c43
guxiaojie <guxiaojie@guxiaojiedeMacBook-Pro.local>
2020-10-23 10:26:05 +0800

    init

---
+++ data.sh
@@ -0,0 +1,11 @@
+options=(
+        "this is one"
+        "this is two"
+        "this is three"
+)
+
+handlers=(
+        "echo 'you have selected  one'"
+        "echo 'you have selected  two'"
+        "echo 'you have selected  three'"
+)
